1e9a1b54c1ff54231ce4f4374ec2377a8a894f4a
Rafay Qureshi <rafayyqureshi@gmail.com>
2025-05-09 16:51:08 +0500

    Fix client issues: storage display, filename format, remove buttons

--- git-push.bat
+++ git-push.bat
@@ -4,6 +4,9 @@ git config --global user.email "rafayyqureshi@gmail.com"
 git config --global user.name "Rafay Qureshi"
 git add .
 git commit -m "Fix client issues: storage display, filename format, remove buttons"
+echo Pulling changes from remote repository...
+git pull origin main --allow-unrelated-histories
+echo Pushing changes to remote repository...
 git push origin main
 echo Done!
 pause 
